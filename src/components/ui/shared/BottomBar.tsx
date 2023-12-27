eb2b95fb7ffdfa9e572190270914908bf38a903f
Guillermo-prog <72439217+Guillermo-prog@users.noreply.github.com>
2023-12-27 14:35:19 +0100

    feat: bottombar created

---
+++ src/components/ui/shared/BottomBar.tsx
@@ -0,0 +1,32 @@
+import { bottombarLinks } from "@/constants";
+import { useLocation, Link } from "react-router-dom";
+
+const BottomBar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`bottombar-link ${
+              isActive && "bg-primary-500 rounded-[10px]"
+            }`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              className={`${isActive && "invert-white"}`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default BottomBar;
